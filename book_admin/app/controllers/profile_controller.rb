7317bc05adb1bd13d356fea467fbba4b5a6a0ae5
ssk333 <sasaki120ml@gmail.com>
2016-09-09 03:08:37 +0900

    StrongParametersの実装
    Hashのkeyの事前検査をします．

--- book_admin/app/controllers/profile_controller.rb
+++ book_admin/app/controllers/profile_controller.rb
@@ -8,6 +8,13 @@ class ProfileController < ApplicationController
   def update
     user = current_user
     # params[:user] => {name: "bob",email: "bob@example.com"}
-    user.update(params[:user])
+    user.update(user_params)
+  end
+
+  private
+  # 外部から渡されるパラメータをそのまま信用せず，
+  # ホワイトリスト方式で検査する．
+  def user_params
+    params.require(:user).permit(:name,:email)
   end
 end
