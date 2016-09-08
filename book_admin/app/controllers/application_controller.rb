2fe7a17df325da18fe5f0a91fd69fd21297b5177
ssk333 <sasaki120ml@gmail.com>
2016-09-09 03:00:04 +0900

    rescue_formを用いた例外処理の定義

--- book_admin/app/controllers/application_controller.rb
+++ book_admin/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+class LoginFailed < StandardError
+end
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from LoginFailed,with: :login_failed
+
+  def login_failed
+    render template: 'shared/login_failed',status: 401
+  end
 end
