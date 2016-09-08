09b7e5e550b1415af3979189456e25803fc9ef23
ssk333 <sasaki120ml@gmail.com>
2016-09-09 00:56:37 +0900

    enum型/列挙型を扱う
    文字列と数値を関連付けて扱いやすくしています．

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  enum status: %w(reservation now_on_sale_ end_of_print)
+
   default_scope -> {order("published_on desc")}
   scope :costly, -> {where("price > ? ",3000)}
   scope :written_about, ->(theme) {where("name like ?","%#{theme}%")}
--- book_admin/db/schema.rb
+++ book_admin/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908082609) do
+ActiveRecord::Schema.define(version: 20160908154215) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160908082609) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "publisher_id"
+    t.integer  "status"
     t.index ["publisher_id"], name: "index_books_on_publisher_id", using: :btree
   end
 
