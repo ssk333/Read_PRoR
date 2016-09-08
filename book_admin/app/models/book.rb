bc2a53266ff0d8f8293b9f24c846cecd6bce64e8
ssk333 <sasaki120ml@gmail.com>
2016-09-09 00:14:38 +0900

    バリデーションの定義
    ５文字以上２０文字以下みたいな制限を設けるものです．

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -7,4 +7,8 @@ class Book < ApplicationRecord
 
   has_many :book_authors
   has_many :authors,through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: {maximum: 15}
+  validates :price, numericality:{greater_than_or_equal_to:0}
 end
