ead0f0bb87f4d9cd242b4d2e71c2be34a8b0c257
ssk333 <sasaki120ml@gmail.com>
2016-09-09 04:00:31 +0900

    なんかエラーでるからコメントアウト
    今の処理と関係なさそうなところコメントアウトしました．

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
-  before_action :require_login
+  # before_action :set_book, only: [:show, :edit, :update, :destroy]
+  # before_action :require_login
 
-  def require_login
-    #ログインを確認し，非ログイン時にリダイレクトする処理
-  end
+  # def require_login
+  #   #ログインを確認し，非ログイン時にリダイレクトする処理
+  # end
 
   def show
     @book = Book.find(params[:id])
@@ -12,18 +12,18 @@ class BooksController < ApplicationController
     #   format.html
     #   format.csv
     #   format.json
+    # end
     render :show
-    end
   end
 
-  private
-  def set_book
-    @book = Book.find(params[:id])
-  end
+#   private
+#   def set_book
+#     @book = Book.find(params[:id])
+#   end
 end
 
-class LoginController < ApplicationController
-  # 「ログインをするための画面」にログインが必要では
-  # いつまでもログインが出来ないので，require_loginをスキップする．
-  skip_before_action :require_login, only:[:new,:create]
-end
+# class LoginController < ApplicationController
+#   # 「ログインをするための画面」にログインが必要では
+#   # いつまでもログインが出来ないので，require_loginをスキップする．
+#   skip_before_action :require_login, only:[:new,:create]
+# end
--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -10,29 +10,29 @@ class Book < ApplicationRecord
   has_many :book_authors
   has_many :authors,through: :book_authors
 
-  validates :name, presence: true
-  validates :name, length: {maximum: 15}
-  validates :price, numericality:{greater_than_or_equal_to:0}
-  validates do |book|
-    if book.name.include?("exercise")
-      book.errors[:name] << "I don't like exercise."
-    end
-  end
-
-  before_validation do |book|
-    book.name = self.name.gsub(/Cat/) do |matched|
-      "lovely #{matched}"
-    end
-  end
-  after_destroy do |book|
-    Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
-  end
-
-  def high_price?
-    price >= 5000
-  end
-  after_destroy :if => :high_price? do |book|
-    Rails.logger.warn "Book with high price is deleted: #{book.attributes.inspect}"
-    Rails.logger.warn "Please check!!"
-  end
+  # validates :name, presence: true
+  # validates :name, length: {maximum: 15}
+  # validates :price, numericality:{greater_than_or_equal_to:0}
+  # validates do |book|
+  #   if book.name.include?("exercise")
+  #     book.errors[:name] << "I don't like exercise."
+  #   end
+  # end
+  #
+  # before_validation do |book|
+  #   book.name = self.name.gsub(/Cat/) do |matched|
+  #     "lovely #{matched}"
+  #   end
+  # end
+  # after_destroy do |book|
+  #   Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
+  # end
+  #
+  # def high_price?
+  #   price >= 5000
+  # end
+  # after_destroy :if => :high_price? do |book|
+  #   Rails.logger.warn "Book with high price is deleted: #{book.attributes.inspect}"
+  #   Rails.logger.warn "Please check!!"
+  # end
 end
