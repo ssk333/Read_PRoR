33356972df4060ecdefbaf000ae6f1497c1e2328
ssk333 <sasaki120ml@gmail.com>
2016-09-08 17:48:00 +0900

    モデル同士のリレーション
    PublisherとBooksに１対多，
    BookとAuthorに多対多の関係を持たせました．

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,5 +1,10 @@
 class Book < ApplicationRecord
-  default_scope -> {order("published_on desc")} 
+  default_scope -> {order("published_on desc")}
   scope :costly, -> {where("price > ? ",3000)}
   scope :written_about, ->(theme) {where("name like ?","%#{theme}%")}
+
+  belongs_to :publisher
+
+  has_many :book_authors
+  has_many :authors,through: :book_authors
 end
--- book_admin/db/schema.rb
+++ book_admin/db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908060520) do
+ActiveRecord::Schema.define(version: 20160908082609) do
+
+  create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "penname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "book_id"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id", using: :btree
+    t.index ["book_id"], name: "index_book_authors_on_book_id", using: :btree
+  end
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -19,6 +35,18 @@ ActiveRecord::Schema.define(version: 20160908060520) do
     t.integer  "number_of_page"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "publisher_id"
+    t.index ["publisher_id"], name: "index_books_on_publisher_id", using: :btree
   end
 
+  create_table "publishers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.text     "address",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
+  add_foreign_key "books", "publishers"
 end
