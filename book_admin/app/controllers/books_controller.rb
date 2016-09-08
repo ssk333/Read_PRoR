cd8266003475b4c276a9de21a8613bb55d0dcbd7
ssk333 <sasaki120ml@gmail.com>
2016-09-09 01:43:22 +0900

    callbackのスキップ処理の定義
    内容はコメントのみにし，省略してある．

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :require_login
+
+  def require_login
+    #ログインを確認し，非ログイン時にリダイレクトする処理
+  end
 
   def show
     @book = Book.find(params[:id])
@@ -14,4 +19,10 @@ class BooksController < ApplicationController
   def set_book
     @book = Book.find(params[:id])
   end
+end
+
+class LoginController < ApplicationController
+  # 「ログインをするための画面」にログインが必要では
+  # いつまでもログインが出来ないので，require_loginをスキップする．
+  skip_before_action :require_login, only:[:new,:create]
 end
