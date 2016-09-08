ce3555ef326b7fb431af9b7208e5d550a1738f74
ssk333 <sasaki120ml@gmail.com>
2016-09-08 16:51:30 +0900

    scopeの定義
    ・"高価"で本を見つけるScope
    ・”高価”，”テーマ”で本を見つけるScope
    ・デフォルトで”発売日降順”にするScope

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  default_scope -> {order("published_on desc")} 
+  scope :costly, -> {where("price > ? ",3000)}
+  scope :written_about, ->(theme) {where("name like ?","%#{theme}%")}
 end
