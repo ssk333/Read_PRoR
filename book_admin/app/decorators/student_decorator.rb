f0ae69d3dc25304b92983b82352ab01cb4c9588d
ssk333 <sasaki120ml@gmail.com>
2016-09-10 01:08:28 +0900

    mailtoリンクの実装
    モデルの実装を直接汚すこと無くモデルの属性を活かしたビュー用のメソッドを定義できました.

--- book_admin/app/decorators/student_decorator.rb
+++ book_admin/app/decorators/student_decorator.rb
@@ -1,2 +1,9 @@
 module StudentDecorator
+  def email_link(body = nil,&block)
+    if block_given?
+      link_to("mailto:#{email}",&block)
+    else
+      link_to(body, "mailto:#{email}")
+    end
+  end
 end
