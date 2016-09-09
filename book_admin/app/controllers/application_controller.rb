6a36b6d11c9a61d75cb665a9e8d0757f2c62e957
ssk333 <sasaki120ml@gmail.com>
2016-09-09 17:09:36 +0900

    モバイル用ページの作成
    モバイル端末から接続したら，モバイル用のページを表示するようにしました．

--- book_admin/app/controllers/application_controller.rb
+++ book_admin/app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class LoginFailed < StandardError
 end
 
 class ApplicationController < ActionController::Base
+  before_action :detect_mobile_variant
   protect_from_forgery with: :exception
   rescue_from LoginFailed,with: :login_failed
 
   def login_failed
     render template: 'shared/login_failed',status: 401
+  end
+
+  private
+  def detect_mobile_variant
+    request.variant = :mobile if request.user_agent =~ /iPhone/
   end
 end
