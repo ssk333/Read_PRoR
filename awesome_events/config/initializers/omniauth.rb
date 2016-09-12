15a3e5d173461a90c93ba52957005ce0f6fcc13f
ssk333 <sasaki120ml@gmail.com>
2016-09-13 00:37:00 +0900

    スペル修正

--- awesome_events/config/initializers/omniauth.rb
+++ awesome_events/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use Omniaauth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,
     Rails.application.secrets.twitter_api_key,
     Rails.application.secrets.twitter_api_secret
