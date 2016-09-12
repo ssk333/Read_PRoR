ddd5c494f85a73ffbe22ce6fcf2be723e3c708b3
ssk333 <sasaki120ml@gmail.com>
2016-09-13 02:15:12 +0900

    認証後のログインメッセージの表示

--- awesome_events/app/controllers/sessions_controller.rb
+++ awesome_events/app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
-    sessions[:user_id] = uer.id
+    session[:user_id] = user.id
     redirect_to root_path,notice: 'ログインしました'
   end
 end
--- awesome_events/config/routes.rb
+++ awesome_events/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+  get '/auth/:provider/callback' => 'sessions#create'
 
 
 end
