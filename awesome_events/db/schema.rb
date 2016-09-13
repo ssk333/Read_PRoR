9b20dc197371653934a7597d4c33c14d7f6a9802
ssk333 <sasaki120ml@gmail.com>
2016-09-13 23:58:35 +0900

    イベント用のモデルを作成

--- awesome_events/db/schema.rb
+++ awesome_events/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912153417) do
+ActiveRecord::Schema.define(version: 20160913145113) do
+
+  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "owner_id"
+    t.string   "name",                     null: false
+    t.string   "place",                    null: false
+    t.datetime "start_time",               null: false
+    t.datetime "end_time",                 null: false
+    t.text     "content",    limit: 65535, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["owner_id"], name: "index_events_on_owner_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider",   null: false
