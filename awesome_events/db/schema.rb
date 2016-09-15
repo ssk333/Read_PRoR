31cd328f0fbf5e72eb3b44791f8d36911406421a
ssk333 <sasaki120ml@gmail.com>
2016-09-16 01:11:13 +0900

    イベント参加用モデルとコントローラの作成

--- awesome_events/db/schema.rb
+++ awesome_events/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913145113) do
+ActiveRecord::Schema.define(version: 20160915155155) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "owner_id"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 20160913145113) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.index ["owner_id"], name: "index_events_on_owner_id", using: :btree
+  end
+
+  create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "event_id",   null: false
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id", "user_id"], name: "index_tickets_on_event_id_and_user_id", unique: true, using: :btree
+    t.index ["event_id"], name: "index_tickets_on_event_id", using: :btree
+    t.index ["user_id", "event_id"], name: "index_tickets_on_user_id_and_event_id", unique: true, using: :btree
+    t.index ["user_id"], name: "index_tickets_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
