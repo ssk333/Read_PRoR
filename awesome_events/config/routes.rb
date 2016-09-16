69a4a538c49ff16892e59ec01b9c0e321a6ab43a
ssk333 <sasaki120ml@gmail.com>
2016-09-16 22:07:05 +0900

    ticketsのルーティング

--- awesome_events/config/routes.rb
+++ awesome_events/config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy',as: :logout
 
+  resources :events do
+    resources :tickets
+  end
 end
