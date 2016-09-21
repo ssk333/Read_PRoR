7fbae131bd37718f974723d312fcc9324d264213
ssk333 <sasaki120ml@gmail.com>
2016-09-21 20:02:06 +0900

    イベント参加キャンセルの実装

--- awesome_events/app/controllers/tickets_controller.rb
+++ awesome_events/app/controllers/tickets_controller.rb
@@ -17,4 +17,10 @@ class TicketsController < ApplicationController
       render json: { messages: ticket.errors.full_messages },status:422
     end
   end
+
+  def destroy
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
+    ticket.destroy!
+    redirect_to event_path(params[:event_id]), notice: 'このイベントの参加をキャンセルしました'
+  end
 end
